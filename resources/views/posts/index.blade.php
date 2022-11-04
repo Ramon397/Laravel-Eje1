38f9e0ce3be5d637fc05b2a11c9e23902a94e65f
Ramon397 <ramonpioparra@gmail.com>
2022-11-04 15:55:06 -0700

    CAMBIOS VISUALES

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,10 +4,7 @@
 >
     <header class="px-6 py-4 space-y-2 text-center">
         <h1 class="font-serif text-3xl text-sky-600 dark:text-sky-500">Blog</h1>
-
-        @auth
-            <a class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-center text-white uppercase transition duration-150 ease-in-out border border-transparent rounded-md dark:text-sky-200 bg-sky-800 hover:bg-sky-700 active:bg-sky-900 focus:outline-none focus:border-sky-900 focus:shadow-outline-sky" href="{{ route('posts.create') }}">Create new post</a>
-        @endauth
+        <a class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-center text-white uppercase transition duration-150 ease-in-out border border-transparent rounded-md dark:text-sky-200 bg-sky-800 hover:bg-sky-700 active:bg-sky-900 focus:outline-none focus:border-sky-900 focus:shadow-outline-sky" href="{{ route('posts.create') }}">Create new post</a>
     </header>
     <main class="grid w-full gap-4 px-4 max-w-7xl sm:grid-cols-2 md:grid-cols-3">
         @foreach($post as $post)
